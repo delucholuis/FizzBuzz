16639798d5f1f8cbc6e61a818322ae29b5419b19
Luis Lopez <delucholuis@gmail.com>
2017-11-17 11:00:49 -0800

    FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 100;
+            int i = 1;
+
+            while (i <= Number){
+                Console.WriteLine(i + (i % 3 == 0 ? " Fizz": (i % 5 == 0 ? " Buzz": "")));
+                /*if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                if(i % 3 == 0 ){
+                    Console.WriteLine("Fizz");
+                } 
+                else if (i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 15 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else {
+                   Console.WriteLine(i);
+                } */
+                i++;
+            }
+
+        }
+    }
+}
